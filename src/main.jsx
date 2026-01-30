5cf96cb91a643995fa7eeb8f2475f1e364f731e5
Nobody <orangnoob6@gmail.com>
2026-01-30 23:38:00 +0700

    Remove web-vitals dependency (not installed)

--- src/main.jsx
+++ src/main.jsx
@@ -5,21 +5,6 @@ import ErrorBoundary from '@components/common/ErrorBoundary'
 import './styles/index.css'
 import './locales/i18n'
 
-// Performance: Report web vitals
-const reportWebVitals = (onPerfEntry) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry)
-      getFID(onPerfEntry)
-      getFCP(onPerfEntry)
-      getLCP(onPerfEntry)
-      getTTFB(onPerfEntry)
-    }).catch(() => {
-      // web-vitals not installed, skip
-    })
-  }
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary>
@@ -27,8 +12,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </ErrorBoundary>
   </React.StrictMode>,
 )
-
-// Log performance metrics in development
-if (import.meta.env.DEV) {
-  reportWebVitals(console.log)
-}
